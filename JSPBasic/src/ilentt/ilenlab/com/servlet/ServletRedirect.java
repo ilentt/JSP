3f54024b16262473ff27a82419b2dfc7e9fb3e27
ilentt <ilentt@ilenlab.com>
2015-09-13 00:34:23 +0700

    commit servlet redirect

--- JSPBasic/src/ilentt/ilenlab/com/servlet/ServletRedirect.java
+++ JSPBasic/src/ilentt/ilenlab/com/servlet/ServletRedirect.java
@@ -1,17 +1,13 @@
 package ilentt.ilenlab.com.servlet;
 
-import ilentt.ilenlab.com.jsp.beans.Constants;
-
 import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/other/redirect")
 public class ServletRedirect extends HttpServlet {
 
 	/**
@@ -20,10 +16,18 @@ public class ServletRedirect extends HttpServlet {
 	private static final long serialVersionUID = -3011658611697275986L;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String value = (String)request.getAttribute(Constants.ATTRIBUTE_USER_NAME_KEY);
+		String redirect = request.getParameter("redirect");
+		if("true".equalsIgnoreCase(redirect)) {
+			System.out.println("Redirect to show redirect base");
+			
+			String contextPath = request.getContextPath();
+			response.sendRedirect(contextPath + "/other/redirect");
+			return;
+		}
+		
 		ServletOutputStream out = response.getOutputStream();
-		out.println("<h1>Redirect Servlet</h1>");
-		out.println(value);
+		out.println("<h3>Text of redirect demo</h3>");
+		out.println(" - servlet path = " + request.getServletPath());
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
