bca2744313304a445356e0eb77a50657c65715df
ilentt <ilentt@ilenlab.com>
2015-09-13 00:39:33 +0700

    update servlet redirect

--- JSPBasic/src/ilentt/ilenlab/com/servlet/ServletRedirect.java
+++ JSPBasic/src/ilentt/ilenlab/com/servlet/ServletRedirect.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/other/runRedirect")
 public class ServletRedirect extends HttpServlet {
 
 	/**
