b078e567c9dfaaa366283f59df56f3e6e5b219e5
ilentt <ilentt@ilenlab.com>
2015-09-14 03:32:25 +0700

    update project

--- JSPAPP/src/ilentt/ilenlab/com/servlet/Home.java
+++ JSPAPP/src/ilentt/ilenlab/com/servlet/Home.java
@@ -24,7 +24,7 @@ public class Home extends HttpServlet {
 	// forward đến trang /WEB-INF/Views/home.jsp
 	// người dùng không bao giờ truy cập được trực tiếp vào các trang jsp đặt trong /WEB-INF/
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/view/pages/home.jsp");
+		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/view/home.jsp");
 		dispatcher.forward(request, response);
 	}
 	
