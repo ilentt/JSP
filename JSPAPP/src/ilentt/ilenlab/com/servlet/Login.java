6b6a3ceb8eb827e12c1f27e470f5ca055b197b6f
ilentt <ilentt@ilenlab.com>
2015-09-16 23:43:06 +0700

    update project

---
+++ JSPAPP/src/ilentt/ilenlab/com/servlet/Login.java
@@ -0,0 +1,33 @@
+package ilentt.ilenlab.com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"{/login}"})
+public class Login extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6730130141016530281L;
+
+	public Login() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/login.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+}
